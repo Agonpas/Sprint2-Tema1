e3907327cce7bf50a120e44b85eba310acad9070
Agonpas <agonpas@gmail.com>
2023-12-20 21:27:59 +0100

    A ver si ahora si

--- Estructura de datos/Nivel 1/Optica/schema_optica.sql
+++ Estructura de datos/Nivel 1/Optica/schema_optica.sql
@@ -12,7 +12,9 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- Schema optica
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `optica` DEFAULT CHARACTER SET utf8 ;
-
+-- -----------------------------------------------------
+-- Schema universidad
+-- -----------------------------------------------------
 USE `optica` ;
 
 -- -----------------------------------------------------
@@ -21,7 +23,7 @@ USE `optica` ;
 CREATE TABLE IF NOT EXISTS `optica`.`proveedor` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `nombre` VARCHAR(45) NOT NULL,
-  `direccion` VARCHAR(255) NOT NULL,
+  `direccion` VARCHAR(200) NOT NULL,
   `telefono` VARCHAR(45) NOT NULL,
   `fax` VARCHAR(45) NULL,
   `nif` VARCHAR(45) NOT NULL,
@@ -30,80 +32,17 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `optica`.`empleados`
+-- Table `optica`.`marca`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`empleados` (
+CREATE TABLE IF NOT EXISTS `optica`.`marca` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `proveedor_id` INT NOT NULL,
   `nombre` VARCHAR(45) NOT NULL,
-  `apellido1` VARCHAR(45) NOT NULL,
-  `apellido2` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `optica`.`recomendacion`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`recomendacion` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `clientes_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_recomendacion_clientes1_idx` (`clientes_id` ASC) VISIBLE,
-  CONSTRAINT `fk_recomendacion_clientes1`
-    FOREIGN KEY (`clientes_id`)
-    REFERENCES `optica`.`clientes` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `optica`.`clientes`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`clientes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `recomendacion_id` INT NULL,
-  `nombre` VARCHAR(45) NOT NULL,
-  `direccion` VARCHAR(255) NOT NULL,
-  `telefono` VARCHAR(45) NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  `fecha_alta` DATE NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_clientes_recomendacion1_idx` (`recomendacion_id` ASC) VISIBLE,
-  CONSTRAINT `fk_clientes_recomendacion1`
-    FOREIGN KEY (`recomendacion_id`)
-    REFERENCES `optica`.`recomendacion` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `optica`.`ventas`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `optica`.`ventas` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `gafas_id` INT NOT NULL,
-  `empleados_id` INT NOT NULL,
-  `clientes_id` INT NOT NULL,
-  `fecha` DATE NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `gafas_id_idx` (`gafas_id` ASC) VISIBLE,
-  INDEX `empleados_id_idx` (`empleados_id` ASC) VISIBLE,
-  INDEX `clientes_id_idx` (`clientes_id` ASC) VISIBLE,
-  CONSTRAINT `gafas_id`
-    FOREIGN KEY (`gafas_id`)
-    REFERENCES `optica`.`gafas` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `empleados_id`
-    FOREIGN KEY (`empleados_id`)
-    REFERENCES `optica`.`empleados` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `clientes_id`
-    FOREIGN KEY (`clientes_id`)
-    REFERENCES `optica`.`clientes` (`id`)
+  INDEX `fk_marca_proveedor1_idx` (`proveedor_id` ASC) VISIBLE,
+  CONSTRAINT `fk_marca_proveedor1`
+    FOREIGN KEY (`proveedor_id`)
+    REFERENCES `optica`.`proveedor` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -114,64 +53,119 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `optica`.`gafas` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `proveedor_id` INT NOT NULL,
-  `ventas_id` INT NOT NULL,
-  `marca` VARCHAR(45) NOT NULL,
-  `graduacion_dch` INT(5) NOT NULL,
-  `graduacion_izq` INT(5) NOT NULL,
+  `marca_id` INT NOT NULL,
+  `graduacion_dch` DECIMAL(5,2) NOT NULL,
+  `graduacion_izq` DECIMAL(5,2) NOT NULL,
   `montura` ENUM('flotante', 'pasta', 'metalica') NOT NULL,
   `color_montura` VARCHAR(45) NOT NULL,
   `color_cristal_dch` VARCHAR(45) NOT NULL,
   `color_cristal_izq` VARCHAR(45) NOT NULL,
   `precio` FLOAT(6,2) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `proveedor_id_idx` (`proveedor_id` ASC) VISIBLE,
-  INDEX `ventas_id_idx` (`ventas_id` ASC) VISIBLE,
-  CONSTRAINT `proveedor_id`
-    FOREIGN KEY (`proveedor_id`)
-    REFERENCES `optica`.`proveedor` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `ventas_id`
-    FOREIGN KEY (`ventas_id`)
-    REFERENCES `optica`.`ventas` (`id`)
+  INDEX `fk_gafas_marca1_idx` (`marca_id` ASC) VISIBLE,
+  CONSTRAINT `fk_gafas_marca1`
+    FOREIGN KEY (`marca_id`)
+    REFERENCES `optica`.`marca` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
--- Insertamos datos de ejemplo para poder hacer consultas
+
+-- -----------------------------------------------------
+-- Table `optica`.`clientes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica`.`clientes` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `clientes_recomen_id` INT NULL,
+  `nombre` VARCHAR(45) NOT NULL,
+  `direccion` VARCHAR(200) NOT NULL,
+  `telefono` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `fecha_alta` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_clientes_clientes1_idx` (`clientes_recomen_id` ASC) VISIBLE,
+  CONSTRAINT `fk_clientes_clientes1`
+    FOREIGN KEY (`clientes_recomen_id`)
+    REFERENCES `optica`.`clientes` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `optica`.`empleado`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica`.`empleado` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(45) NOT NULL,
+  `apellido1` VARCHAR(45) NOT NULL,
+  `apellido2` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `optica`.`ventas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `optica`.`ventas` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `empleado_id` INT NOT NULL,
+  `cliente_id` INT NOT NULL,
+  `gafas_id` INT NOT NULL,
+  `fecha` DATE NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `empleados_id_idx` (`empleado_id` ASC) VISIBLE,
+  INDEX `clientes_id_idx` (`cliente_id` ASC) VISIBLE,
+  INDEX `fk_ventas_gafas1_idx` (`gafas_id` ASC) VISIBLE,
+  CONSTRAINT `empleado_id`
+    FOREIGN KEY (`empleado_id`)
+    REFERENCES `optica`.`empleado` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `cliente_id`
+    FOREIGN KEY (`cliente_id`)
+    REFERENCES `optica`.`clientes` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ventas_gafas1`
+    FOREIGN KEY (`gafas_id`)
+    REFERENCES `optica`.`gafas` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
 INSERT INTO optica.proveedor (nombre, direccion, telefono, fax, nif)
 VALUES 
 	('Cristal', 'Calle Principal Nº 1, bajos, 08015 Barcelona España', '934550986', '934550986', '43948598E'),
     ('Visión', 'Calle Crucero Nº 22, Nave3, 31001 Pamplona España', '9482325466', NULL, '25948548A');
-
-INSERT INTO optica.empleados (nombre, apellido1, apellido2)
+    
+INSERT INTO optica.marca (proveedor_id, nombre)
+VALUES
+    (1, 'Rayan'),
+    (2, 'Toñi');
+    
+INSERT INTO optica.gafas (marca_id, graduacion_dch, graduacion_izq, montura, color_montura, color_cristal_dch, color_cristal_izq, precio)
+VALUES
+    (1, 12.25, 10.50, 'flotante', 'Negro', 'Azul', 'Verde', 150.00),
+    (2, 5.15, 7.75, 'pasta', 'Rojo', 'Amarillo', 'Azul', 200.00);
+    
+    
+INSERT INTO optica.empleado (nombre, apellido1, apellido2)
 VALUES
 	('Celia', 'Monzón', 'Gutierrez'),
     ('Marcos', 'Martorell', 'Montes');
     
-INSERT INTO optica.clientes (recomendacion_id, nombre, direccion, telefono, email, fecha_alta)
+INSERT INTO optica.clientes (clientes_recomen_id, nombre, direccion, telefono, email, fecha_alta)
 VALUES 
 	(NULL, 'Pedro', 'Calle Fontanals 45 1º 5ª 08012 Barcelona', '934566543', 'pedro@emali.com', '2023-01-01'), 
     (1, 'Asunción', 'Calle Rentas 33 Atico 3ª 08222 Terrassa', '934464544', 'asuncion@emali.com', '2023-01-01');
     
-INSERT INTO optica.gafas (proveedor_id, ventas_id, marca, graduacion_dch, graduacion_izq, montura, color_montura, color_cristal_dch, color_cristal_izq, precio)
-VALUES
-    (1, 1, 'Rayan', 12, 10, 'flotante', 'Negro', 'Azul', 'Verde', 150.00),
-    (2, 2, 'Toñi', 5, 7, 'pasta', 'Rojo', 'Amarillo', 'Azul', 200.00);
-    
-INSERT INTO optica.recomendacion (clientes_id)
-VALUES
-    (1),
-    (2);
-    
-INSERT INTO optica.ventas (gafas_id, empleados_id, clientes_id, fecha)
+INSERT INTO optica.ventas (empleado_id, cliente_id, gafas_id, fecha)
 VALUES
     (1, 1, 1, '2023-01-03'),
+    (1, 1, 2, '2023-01-03'),
     (2, 2, 2, '2023-01-04');
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
